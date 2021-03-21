78e95a3193f8ead68522964a1066387c36a8bdc8
Filipe Pimentel Pinto <filipepimentel.fp@gmail.com>
2021-03-21 15:35:58 -0300

    ajustes pre publish

--- SimplePayMPModule/src/main/java/com/df/simplepaympmodule/di/AppM.kt
+++ SimplePayMPModule/src/main/java/com/df/simplepaympmodule/di/AppM.kt
@@ -5,12 +5,12 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
-class App : Application() {
+class AppM : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin{
             androidLogger()
-            androidContext(this@App)
+            androidContext(this@AppM)
             koin.loadModules(
                 listOf(
                     apisModule,
